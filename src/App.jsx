90c4bcd6ee3a8a7c6319a12eb53d03c0871d9b13
millionairesmile <lswoo1221@gmail.com>
2023-11-07 17:38:37 +0900

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import DateData from "./date.json";
 
 const App = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center">
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="mb-12 text-2xl font-bold">2023년 11월</h1>
       <ul className="grid grid-cols-7 gap-4">
         <li className="w-32 h-32"></li>
         <li className="w-32 h-32"></li>
